2e5de29abb25c05315a9a2288ed0893a7515c7a1
Imran <imrau99@hotmail.co.uk>
2022-02-07 15:39:46 +0000

    fixed updateById

--- src/main/java/controller/PersonController.java
+++ src/main/java/controller/PersonController.java
@@ -44,7 +44,7 @@ public class PersonController {
     //    Person updateById(Long id, Person updatedPerson);
     @PutMapping(path = "/{id}")
     public ResponseEntity updateById(@PathVariable Long id, @RequestBody Person person) {
-        Person returnValue = this.personService.deleteById(id);
+        Person returnValue = this.personService.updateById(id, person);
         if (returnValue != null) {
             return new ResponseEntity<>(returnValue, HttpStatus.ACCEPTED);
         } else {
