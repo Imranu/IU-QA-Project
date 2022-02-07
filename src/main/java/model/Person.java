826de6fafb6815c012dd1fbea0f1367167c9034e
Imran <imrau99@hotmail.co.uk>
2022-02-07 12:31:36 +0000

    added some attributes to model

--- src/main/java/model/Person.java
+++ src/main/java/model/Person.java
@@ -1,4 +1,23 @@
 package model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
 public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private LocalDate dateOfBirth;
+
+
+
 }
